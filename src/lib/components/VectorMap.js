3813ecd241b3a26f52da870f7e9c818d942480bb
kadoshms <kadoshms@gmail.com>
2017-10-28 18:47:42 +0300

    added all supported proptyes

--- src/lib/components/VectorMap.js
+++ src/lib/components/VectorMap.js
@@ -58,7 +58,53 @@ class VectorMap extends React.PureComponent {
 
 VectorMap.propTypes = {
     map: PropTypes.oneOf(maps).isRequired,
-    backgroundColor: PropTypes.string
+    backgroundColor: PropTypes.string,
+    zoomOnScroll: PropTypes.bool,
+    zoomOnScrollSpeed: PropTypes.bool,
+    panOnDrag: PropTypes.bool,
+    zoomMax: PropTypes.number,
+    zoomMin: PropTypes.number,
+    zoomStep: PropTypes.number,
+    zoomAnimate: PropTypes.bool,
+    regionsSelectable: PropTypes.bool,
+    regionsSelectableOne: PropTypes.bool,
+    markersSelectable: PropTypes.bool,
+    markersSelectableOne: PropTypes.bool,
+    regionStyle: PropTypes.object,
+    regionLabelStyle: PropTypes.object,
+    markerStyle: PropTypes.object,
+    markerLabelStyle: PropTypes.object,
+    markers: PropTypes.oneOfType([
+        PropTypes.object,
+        PropTypes.array
+    ]),
+    series: PropTypes.object,
+    focusOn: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object
+    ]),
+    labels: PropTypes.object,
+    selectedRegions: PropTypes.oneOfType([
+        PropTypes.array,
+        PropTypes.object,
+        PropTypes.string
+    ]),
+    selectedMarkers: PropTypes.oneOfType([
+        PropTypes.array,
+        PropTypes.object,
+        PropTypes.string
+    ]),
+    onRegionTipShow: PropTypes.func,
+    onRegionOver: PropTypes.func,
+    onRegionOut: PropTypes.func,
+    onRegionClick: PropTypes.func,
+    onRegionSelected: PropTypes.func,
+    onMarkerTipShow: PropTypes.func,
+    onMarkerOver: PropTypes.func,
+    onMarkerOut: PropTypes.func,
+    onMarkerClick: PropTypes.func,
+    onMarkerSelected: PropTypes.func,
+    onViewportChange: PropTypes.func
 };
 
 export default VectorMap;
