5d05f1f033a0bb52564c4005afc08ddaa523aa99
Mor Kadosh <kadoshms@gmail.com>
2021-09-18 11:47:54 +0300

    chore: added commit-message git hook

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,5 @@
 module.exports = {
     extends: [
-        "@commitlint/config-conventional",
-        "@commitlint/config-lerna-scopes",
-    ],
-    rules: {
-        "scope-empty": [2, "never"],
-        "scope-enum": [0, "always"],
-    },
+        "@commitlint/config-conventional"
+    ]
 };
