f2ab134965e499433909144644e8d82a7ed9a2d0
Mor Kadosh <kadoshms@gmail.com>
2021-09-24 10:44:33 +0300

    chore: added many types to the map interface

--- packages/core/src/components/VectorMap/types.ts
+++ packages/core/src/components/VectorMap/types.ts
@@ -34,9 +34,53 @@ export interface IVectorMapProps {
    */
   zoomMax?: number;
   /**
+   * Indicates the multiplier used to zoom map with +/- buttons. Default value is 1.6.
+   */
+  zoomStep?: number;
+  /**
+   * When set to true map could be zoomed using mouse scroll. Default value is true.
+   */
+  zoomOnScroll?: boolean;
+  /**
+   * Indicates whether or not to animate changing of map zoom with zoom buttons.
+   */
+  zoomAnimate?: boolean;
+  /**
    * This parameter sets the initial position and scale of the map viewport.
    */
-  focusOn: IFocus;
+  focusOn?: IFocus;
+  /**
+   * When set to true, the map pans when being dragged. Default value is true.
+   */
+  panOnDrag?: boolean;
+  /**
+   * When set to true regions of the map could be selected. Default value is false.
+   */
+  regionsSelectable?: boolean;
+  /**
+   * Allow only one region to be selected at the moment. Default value is false.
+   */
+  regionsSelectableOne?: boolean;
+  /**
+   * When set to true markers on the map could be selected. Default value is false.
+   */
+  markersSelectable?: boolean;
+  /**
+   * Allow only one marker to be selected at the moment. Default value is false.
+   */
+  markersSelectableOne?: boolean;
+  /**
+   * Set the styles for the map's regions. Each region or marker has four states: initial (default state), hover (when the mouse cursor is over the region or marker), selected (when region or marker is selected), selectedHover (when the mouse cursor is over the region or marker and it's selected simultaneously). Styles could be set for each of this states.
+   */
+  regionStyle?: ISVGElementStyleAttributes;
+  /**
+   * Set the styles for the regions' labels. Each region or marker has four states: initial (default state), hover (when the mouse cursor is over the region or marker), selected (when region or marker is selected), selectedHover (when the mouse cursor is over the region or marker and it's selected simultaneously). Styles could be set for each of this states.
+   */
+  regionLabelStyle?: ISVGElementStyleAttributes;
+  /**
+   * Set the styles for the map's markers. Any parameter suitable for regionStyle could be used as well as numeric parameter r to set the marker's radius.
+   */
+  markerStyle?: ISVGElementStyleAttributes | IImageElementStyleAttributes;
 }
 
 interface IBBox {
@@ -73,6 +117,18 @@ interface IFocus {
   lat?: number;
   lng?: number;
   animate?: boolean;
+}
+
+interface ISVGElementStyleAttributes {
+  initial?: CSSProperties;
+  hover?: CSSProperties;
+  selected?: CSSProperties;
+  selectedHover?: CSSProperties;
+}
+
+interface IImageElementStyleAttributes {
+  url: string;
+  offset?: number[];
 }
 
 type PathsDefinition = { [key: string]: { path: string; name: string } };
