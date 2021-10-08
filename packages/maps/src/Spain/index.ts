21a5d1f0910223eeadd217313748d5458adba21a
Mor Kadosh <kadoshms@gmail.com>
2021-10-08 17:48:48 +0300

    fix: added spain map

---
+++ packages/maps/src/Spain/index.ts
@@ -0,0 +1,1 @@
+export { default as esMill } from "./Miller";
--- packages/maps/src/index.ts
+++ packages/maps/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./France";
 export * from "./Germany";
+export * from "./Spain";
 export * from "./USA";
 export * from "./World";
