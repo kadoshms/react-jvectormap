a6f2443da04ade07fb63a8e605ea5063f590b364
Mor Kadosh <kadoshms@gmail.com>
2021-10-30 15:31:52 +0300

    fix: fixed module load

--- packages/jquery-mousewheel/jquery.mousewheel.js
+++ packages/jquery-mousewheel/jquery.mousewheel.js
@@ -5,16 +5,7 @@
 
 export const loadJQueryMouseWheel = ($) =>
   (function (factory) {
-    if (typeof define === "function" && define.amd) {
-      // AMD. Register as an anonymous module.
-      define(["jquery"], factory);
-    } else if (typeof exports === "object") {
-      // Node/CommonJS style for Browserify
-      module.exports = factory;
-    } else {
-      // Browser globals
-      factory($);
-    }
+    factory($);
   })(function ($) {
     var toFix = [
         "wheel",
