f103760556ea57b714f0d0f19915803610d16cd8
Mor Kadosh <kadoshms@gmail.com>
2021-10-30 14:39:15 +0300

    fix: fixed error when transform is nan

--- packages/jvectormap/src/svg-canvas-element.js
+++ packages/jvectormap/src/svg-canvas-element.js
@@ -1,26 +1,35 @@
-jvm.SVGCanvasElement = function(container, width, height){
-  this.classPrefix = 'SVG';
-  jvm.SVGCanvasElement.parentClass.call(this, 'svg');
+jvm.SVGCanvasElement = function (container, width, height) {
+  this.classPrefix = "SVG";
+  jvm.SVGCanvasElement.parentClass.call(this, "svg");
 
-  this.defsElement = new jvm.SVGElement('defs');
-  this.node.appendChild( this.defsElement.node );
+  this.defsElement = new jvm.SVGElement("defs");
+  this.node.appendChild(this.defsElement.node);
 
   jvm.AbstractCanvasElement.apply(this, arguments);
-}
+};
 
 jvm.inherits(jvm.SVGCanvasElement, jvm.SVGElement);
 jvm.mixin(jvm.SVGCanvasElement, jvm.AbstractCanvasElement);
 
-jvm.SVGCanvasElement.prototype.setSize = function(width, height){
+jvm.SVGCanvasElement.prototype.setSize = function (width, height) {
   this.width = width;
   this.height = height;
-  this.node.setAttribute('width', width);
-  this.node.setAttribute('height', height);
+  this.node.setAttribute("width", width);
+  this.node.setAttribute("height", height);
 };
 
-jvm.SVGCanvasElement.prototype.applyTransformParams = function(scale, transX, transY) {
+jvm.SVGCanvasElement.prototype.applyTransformParams = function (
+  scale,
+  transX,
+  transY,
+) {
   this.scale = scale;
   this.transX = transX;
   this.transY = transY;
-  this.rootElement.node.setAttribute('transform', 'scale('+scale+') translate('+transX+', '+transY+')');
+  if (!isNaN(transX) && !isNaN(transY) && !isNaN(scale)) {
+    this.rootElement.node.setAttribute(
+      "transform",
+      "scale(" + scale + ") translate(" + transX + ", " + transY + ")",
+    );
+  }
 };
