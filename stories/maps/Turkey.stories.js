89b6b809ee2cfb74daa5a319bb5ee8929b10c995
Mor Kadosh <kadoshms@gmail.com>
2021-11-04 09:08:20 +0200

    story: fixed turkey story

--- stories/maps/Turkey.stories.js
+++ stories/maps/Turkey.stories.js
@@ -1,23 +1,16 @@
 import { VectorMap } from "@react-jvectormap/core";
-import { thMill, thMerc } from "@react-jvectormap/thailand";
+import { trMill } from "@react-jvectormap/turkey";
 import { MapTemplate } from "../components/MapContainer/MapTemplate";
 
 export default {
-  title: "maps/Map/Thailand",
+  title: "maps/Map/Turkey",
   component: VectorMap,
   argTypes: {},
 };
 
 export const Miller = MapTemplate.bind({});
 Miller.args = {
-  map: thMill,
-  fileName: "thMill",
-  country: "thailand",
-};
-
-export const Mercator = MapTemplate.bind({});
-Mercator.args = {
-  map: thMerc,
-  fileName: "thMerc",
-  country: "thailand",
+  map: trMill,
+  fileName: "trMill",
+  country: "turkey",
 };
