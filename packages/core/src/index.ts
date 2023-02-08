f9c235a033d6ed0844b62a037469e1e2b3e72e9f
Mor Kadosh <mkadosh@paloaltonetworks.com>
2023-02-08 10:43:36 +0200

    chore: test lerna publish

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,3 +4,5 @@ export * from "./components";
 export * from "./utils";
 
 loadJVectorMap($);
+
+// test
--- packages/maps/Austria/index.ts
+++ packages/maps/Austria/index.ts
@@ -2,3 +2,5 @@ import atMerc from "./atMerc.json";
 import atMill from "./atMill.json";
 
 export { atMerc, atMill };
+
+// test
